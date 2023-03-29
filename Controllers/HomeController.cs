56da369d164c4fe0ed8ed3d0d28a3699b7b3ecfb
mnursodik151 <mnursodik151@yahoo.com>
2023-03-29 19:02:39 +0700

    initial commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ChatBotApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        // GET: api/Home
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "Hello", "World" };
+        }
+
+        // GET: api/Home/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
+        {
+            return $"Helo {id} World";
+        }
+
+        // POST: api/Home
+        [HttpPost]
+        public IActionResult Post([FromBody] string value)
+        {
+            return Ok(value);
+        }
+    }
+}
