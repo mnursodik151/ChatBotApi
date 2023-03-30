df79aa4c350e765b10a13c7cefc9ae297b5fbce1
mnursodik151 <mnursodik151@yahoo.com>
2023-03-30 22:55:58 +0700

    [Rev] Update api key

--- Program.cs
+++ Program.cs
@@ -1,35 +1,38 @@
-var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-// Retrieve the Telegram bot token from an environment variable
-var telegramBotToken = Environment.GetEnvironmentVariable("TELEGRAM_BOT_TOKEN");
-
-builder.Services.AddHttpClient<ITelegramMessageService, TelegramMessageService>(client =>
+internal class Program
 {
-    client.BaseAddress = new Uri("https://api.telegram.org/");
-});
+    private static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
 
-// builder.Services.AddScoped<ITelegramMessageService, TelegramMessageService>();
+        // Add services to the container.
 
-var app = builder.Build();
+        builder.Services.AddControllers();
+        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
+        builder.Services.AddHttpClient<ITelegramMessageService, TelegramMessageService>(client =>
+        {
+            client.BaseAddress = new Uri("https://api.telegram.org/");
+        });
+
+        // builder.Services.AddScoped<ITelegramMessageService, TelegramMessageService>();
+
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.UseHttpsRedirection();
+
+        app.UseAuthorization();
+
+        app.MapControllers();
+
+        app.Run();
+    }
 }
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
