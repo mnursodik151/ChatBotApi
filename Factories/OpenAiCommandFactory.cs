585a9e95129660e58a88535026511effda29d38d
mnursodik151 <mnursodik151@yahoo.com>
2023-04-01 12:35:35 +0700

    [Temp] log

--- Factories/OpenAiCommandFactory.cs
+++ Factories/OpenAiCommandFactory.cs
@@ -52,13 +52,12 @@ public class OpenAiCommandFactory
         else
         {
             var config = _serviceProvider.GetService<IConfiguration>(); 
-            _logger.LogInformation(input.message.reply_to_message?.text);           
+            _logger.LogInformation(input.message.reply_to_message?.chat?.id.ToString());           
             if (input.message.chat?.id > 0 || 
             input.message.text.Contains("@kkr_ai_bot") || 
             input.message.reply_to_message?.chat.id.ToString() == config["TelegramBotToken"].Substring(0, 10))
             {
-                input.message.text = $"{input.message.reply_to_message?.text} \n {input.message.text ?? string.Empty}";                
-                _logger.LogInformation(input.message.reply_to_message?.text);
+                input.message.text = $"{input.message.reply_to_message?.text} \n {input.message.text ?? string.Empty}";
                 return new ReplyMessageCommand("DM",
                                                    input,
                                                    _logger,
